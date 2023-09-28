f566f60867120f2d46652fc5881df83dfb5ce2f7
Interzoid <bob@interzoid.com>
2023-09-27 17:53:12 -0700

    Update GetFullNameMatchSimKey.java

--- GetFullNameMatchSimKey.java
+++ GetFullNameMatchSimKey.java
@@ -4,7 +4,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;	
 	
-public class GetFullNameMatchSimKey {
+class GetFullNameMatchSimKey {
 // Get API key at www.interzoid.com/register	
 
 	public static void main(String[] args) {
